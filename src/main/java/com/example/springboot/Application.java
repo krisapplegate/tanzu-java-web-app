591bb9c130c07b148523164bbb535ad4732debaf
krisapplegate <kapplegate@vmware.com>
2024-08-07 07:55:55 -0500

    rebase

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,7 +1,11 @@
 package com.example.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
@@ -13,6 +17,21 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+	
 	@Bean
 	public HttpExchangeRepository htttpTraceRepository() {
 		return new InMemoryHttpExchangeRepository();
